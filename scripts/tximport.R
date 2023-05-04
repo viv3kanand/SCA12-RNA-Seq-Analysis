79d4c14396c88a3216268031378710c2e58ef51d
viv3kanand <viv3kanand@gmail.com>
2023-05-04 18:48:29 +0000

    tximport with inferential replicates

---
+++ scripts/tximport.R
@@ -0,0 +1,60 @@
+tximport = function(dir, txdf, DTE=TRUE){
+  require(tximport)
+  require(SummarizedExperiment)
+  
+  files <- file.path(dir, list.files(dir), "quant.sf")
+  names(files) <- list.files(dir)
+  
+  if(all(file.exists(files))){ # check if all files exist
+    print("All files exist in the salmon folder")
+  }
+  
+  if(DTE){
+    txi <- tximport(files, type = "salmon", tx2gene = txdf, txOut = TRUE, )
+  }
+  else {
+    txi <- tximport(files, type = "salmon", tx2gene = txdf)
+  }
+  
+  coldata <- data.frame(names = names(files),
+                        files = files)
+  
+  se <- makeUnrangedSE(txi, coldata, metadata, txdf)
+  
+  return(se)
+  
+  
+  #### from tximeta wrapper for adding Inferential replicates in se because of dplyr filter error ########
+  tximportInfo <- list(version=packageVersion("tximport"),
+                      type=type,
+                      importTime=Sys.time())
+  metadata <- list(tximportInfo=tximportInfo)
+  metadata$countsFromAbundance <- txi$countsFromAbundance
+  
+  
+  makeUnrangedSE <- function(txi, coldata, metadata, txdf) {
+    assays <- txi[c("counts","abundance","length")]
+    # if there are inferential replicates
+    if ("infReps" %in% names(txi)) {
+      infReps <- rearrangeInfReps(txi$infReps)
+      assays <- c(assays, infReps)
+    }
+    
+    SummarizedExperiment(assays=assays,
+                         colData=coldata,
+                         metadata=metadata,
+                         rowData = txdf)
+  }
+  
+  
+  rearrangeInfReps <- function(infReps) {
+    nreps <- ncol(infReps[[1]])
+    stopifnot(all(sapply(infReps, ncol) == nreps))
+    getCols <- function(j,l) do.call(cbind, lapply(seq_along(l), function(k)  l[[k]][,j]))
+    infReps <- lapply(seq_len(nreps), getCols, infReps)
+    names(infReps) <- paste0("infRep",seq_len(nreps))
+    infReps
+  }
+  ######################################################################################################
+}
+
